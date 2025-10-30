9939e8a1ddc33a16c7bca4d47eae5bf3964421ef
SilasNight <15emiliomurray@gmail.com>
2025-10-30 09:29:15 +0200

    untested, Added a way to add a new book added additive functions to get information for the new book get_book_title get_id get_digit
    Added comments to all of those and function defaults.

--- shelf_track.py
+++ shelf_track.py
@@ -65,8 +65,114 @@ def append_books(data: list) -> None:
     db.close()
 
 
-def add_new_book():
-    pass
+def add_new_book() -> None:
+    """
+    This is used to gather all the information of a new book
+    It will later call a different function to add the data to
+    The books database
+    :return: None
+    """
+
+    print("Now adding a new book")
+
+    # Gathering the data
+    book_id = get_id()
+    title = get_book_title()
+    author_id = get_id()
+    quantity = get_digit(question="How many books are you adding?\n")
+
+    # Condensing all the data and adding it to the database
+    data = [book_id, title, author_id, quantity]
+    append_books(data)
+
+
+def get_book_title(old_title: str = None) -> str:
+    """
+    This is used to get the title of a book.
+    Can be used to edit an existing title as well.
+    :return: str The title of the book.
+    """
+    while True:
+
+        # Checking if there is an old title to show as polish
+        if old_title:
+            print(f"The old title was: {old_title}")
+
+        # Keeping the user trapped in this loop until they confirm
+        # That they are happy with the book title.
+        user_input = input("What should the title for the book be?\n").title()
+        print(f"Is this correct: {user_input}")
+        if yes_or_no():
+            return user_input
+
+
+def get_id() -> str:
+    """
+    This is used to get a valid number ID
+    :return: a string of whole numbers
+    """
+
+    satisfied = False
+    user_input = ''
+    while not satisfied:
+        user_input = input("Please type in the ID. It must be a number.\n")
+
+        # Making sure that the user has input something.
+        if len(user_input) == 0:
+            print("Something must be entered.")
+            continue
+
+        # Making sure that the whole string is numbers.
+        for i in range(len(user_input)):
+            if not user_input[i].isdigit():
+                print(f"Character {i+1} is not a number")
+                continue
+
+        satisfied = True
+    return user_input
+
+
+def get_digit(constraint: int = None, question: str = None) -> int:
+    """
+    This function is used to get a positive whole number.
+    If there is a constraint it makes it so that the number has to be
+    smaller than the constraint.
+
+    :param question: This is a changeable question to ask
+    inside the function
+    :param constraint: A limit to how big the number can be.
+    :return: integer selected
+    """
+
+    # Changing the question based on it's existence
+    if question:
+        prompt = question
+    else:
+        prompt = "Please enter a positive whole number.\n"
+
+    potential_output = 0
+    while True:
+        user_input = input(prompt)
+
+        # Making sure it is a valid number and not negative.
+        if not user_input.isdigit():
+            print(f"Error, {user_input} is not a positive whole number.")
+            continue
+
+        # Changing the input to int for more testing
+        potential_output = int(user_input)
+        if potential_output == 0:
+            print("Error the number can't be equal to zero.")
+            continue
+
+        # If there is a constraint this tests it.
+        if constraint:
+            if potential_output > constraint:
+                print(f"Error the number must be equal or less than {constraint}")
+                continue
+        break
+
+    return potential_output
 
 
 def yes_or_no() -> bool:
